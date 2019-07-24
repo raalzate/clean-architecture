ec4b69bd85919508cebcc45b3c299773fd3696e8
Raul .A Alzate <raul.alzate@techandsolve.com>
2019-07-23 20:19:04 -0500

    estructura y validacion para spring boot

---
+++ spring-boot/src/main/java/com/techandsolve/springboot/BootRunSpringBoot.java
@@ -0,0 +1,27 @@
+package com.techandsolve.springboot;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class BootRunSpringBoot {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BootRunSpringBoot.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+    @RestController
+    public static class ApiController {
+        @GetMapping("/")
+        public String getData(){
+            return "Hola Mundo";
+        }
+    }
+
+}
